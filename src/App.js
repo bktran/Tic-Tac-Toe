2744d8ff727ddc749d201b66e4e528d842cbf244
Kiana-Massey <kxm7046@mavs.uta.edu>
2023-08-07 14:51:28 -0700

    push first part of story 2

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@ import './App.css'
 
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
+  const [player, setPlayer] = useState(["p1","p2","p1","p2","p1","p2","p1","p2","p1","p2"])
 
+  let i = 0
 
+  const handleGamePlay = (clickedSquareIndex) => {
+    let updatedSquares = [...squares]
+
+      if (player[i] === "p1") {
+        updatedSquares[clickedSquareIndex]= "❌"
+        setSquares = updatedSquares
+      }
+      i++
+  }
 
   return (
     <>
       <h1 className='title'>Tic Tac Toe</h1>
       <div className='square-board'>
         {squares.map((square, index)=> {
-          return <Square />
+          return <Square square = {squares} index={index} handleGamePlay={handleGamePlay}/>
         })}
       </div>
     </>
